fa60ca239ab3615a10c31a50fbbe6ec5f05c3751
Ilnaz Mannapov <i.m.mannapov@tinkoff.ru>
2024-08-01 11:56:18 +0300

    new fast scroller

--- app/src/main/java/ru/umma/ummaruapp/view/base/Widgets.kt
+++ app/src/main/java/ru/umma/ummaruapp/view/base/Widgets.kt
@@ -1,19 +1,7 @@
 package ru.umma.ummaruapp.view.base
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Slider
-import androidx.compose.material.SliderDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.dp
 import ru.umma.ummaruapp.R
 
 /**
